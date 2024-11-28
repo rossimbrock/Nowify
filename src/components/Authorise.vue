efb305f4bd4ccbdbeb37ee19ffc5494c106077a4
Ross Imbrock <imbrock.7@osu.edu>
2024-11-27 21:54:40 -0500

    auth

--- src/components/Authorise.vue
+++ src/components/Authorise.vue
@@ -199,7 +199,7 @@ export default {
             .substring(5)
         ].join('-')
       )
-      searchParams.append('scope', 'user-read-currently-playing user-read-playback-position user-modify-playback-state')
+      searchParams.append('scope', 'user-modify-playback-state')
 
       return `${this.endpoints.auth}?${searchParams.toString()}`
     }
